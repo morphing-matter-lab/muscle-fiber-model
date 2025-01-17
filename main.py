3bbab6cad90dfb3089e67e6d4958cd7a5c9c175b
David Jourdan <djourdan@berkeley.edu>
2025-01-17 14:19:13 -0800

    aesthetics

--- main.py
+++ main.py
@@ -37,20 +37,16 @@ for i in range(4, 4 + len(holes) * n_circle):
 
 NV, F = fabsim_py.simulate_membrane(V, np.array(B['triangles']), fixed_dofs, 1.5, 0.25)
 ps_mesh = ps.register_surface_mesh("initial", V, F, smooth_shade=True, enabled=False)
-ps_mesh = ps.register_surface_mesh("deformed", NV, F, smooth_shade=True)
+ps_mesh = ps.register_surface_mesh("deformed", NV, F, smooth_shade=True, color=(42/255, 53/255, 213/255))
 
 angles, eigenvalues = fabsim_py.compute_stretch_angles(V, NV[:, :2], F)
+# alignment = eigenvalues[:,0] /  eigenvalues[:,1]
 dirs = np.empty((len(angles), 2))
 for i in range(len(angles)):
   dirs[i, 0] = np.cos(angles[i])
   dirs[i, 1] = np.sin(angles[i])
-# vec = fabsim_py.compute_membrane_energies(V, np.array(B['triangles']), NV, 1.5, 0.5)
 
-alignment = eigenvalues[:,0] /  eigenvalues[:,1]
-
-# ps_mesh.add_scalar_quantity("energy values", vec, defined_on="faces", enabled=True)
-# ps_mesh.add_scalar_quantity("alignment", alignment, defined_on="faces", enabled=True)
-ps_mesh.add_vector_quantity("strain directions", dirs, defined_on="faces", enabled=True)
-ps_mesh.add_vector_quantity("strain directions2", -dirs, defined_on="faces", enabled=True)
+ps_mesh.add_vector_quantity("strain directions", dirs, defined_on="faces", enabled=True, color=(213/255, 202/255, 42/255), length=0.01)
+ps_mesh.add_vector_quantity("strain directions2", -dirs, defined_on="faces", enabled=True, color=(213/255, 202/255, 42/255), length=0.01)
 
 ps.show()
